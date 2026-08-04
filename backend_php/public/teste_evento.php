fce43aeeb337eaa4d2fc87ad1d427784003457e5
Kleber Salvador <klebersalvador@users.noreply.github.com>
2026-08-04 13:17:02 -0300

    Fix facade root in teste_evento.php

--- backend_php/public/teste_evento.php
+++ backend_php/public/teste_evento.php
@@ -2,8 +2,9 @@
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$request = Illuminate\Http\Request::capture();
-$app->instance('request', $request);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
